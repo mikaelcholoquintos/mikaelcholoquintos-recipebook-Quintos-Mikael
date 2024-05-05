2992aae748b5f288868a689f245c8a7f73d6fdc3
mikaelcholoquintos <157821462+mikaelcholoquintos@users.noreply.github.com>
2024-05-06 03:09:06 +0800

    Delete recipe_book_project directory

--- recipe_book_project/recipes/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RecipeListView, RecipeDetailView
-
-urlpatterns = [
-    path('', RecipeListView.as_view(), name='recipe_list'),
-    path('detail/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),
-]
